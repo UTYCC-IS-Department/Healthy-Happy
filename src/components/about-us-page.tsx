3ab6a3de61b4768252240a3b622e07100f573a94
Monopoly4545 <alinhtet410@gmail.com>
2026-07-24 21:22:30 +0630

    make Sustainability.tsx

--- src/components/about-us-page.tsx
+++ src/components/about-us-page.tsx
@@ -3,14 +3,16 @@ import React from "react";
 import Headline from "./about_us_comp/HeadLine";
 import WhereCanBuy from "./about_us_comp/WhereCanBuy";
 import CoreIdentity from "./about_us_comp/CoreIdentity";
+import Sustainability from "./about_us_comp/Sustainability";
 
 
 export function AboutUsPage() {
   return (
     <div>
       <Headline/>
-      
+
       <CoreIdentity/>
+      <Sustainability/>
       <WhereCanBuy/>
     </div>
   );
