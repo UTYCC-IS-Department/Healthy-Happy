fd74e9f7487e67d4733c0ea89ad087987bf6c003
NectarHub <wutyikyaw.2002@gmail.com>
2026-07-29 00:23:42 +0630

    change the nav bar logo

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -25,7 +25,7 @@ export function SiteHeader() {
       <div className="mx-auto flex h-20 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
 
         <Link href="/en" className="flex items-center gap-3" onClick={() => setMobileOpen(false)}>
-          <span className="rounded-md bg-white p-1">
+          <span className=" p-1">
             <Image
               src={assets.logo}
               alt="Healthy & Happy Myanmar Logo"
